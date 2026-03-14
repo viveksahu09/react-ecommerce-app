ba9410a284a3d9b683d33e02ea78dd6d8672d08c
Vsahu <vsahu53891@gmail.com>
2026-03-14 13:01:07 +0530

    Add filters sidebar UI with category, price, and rating using React-Bootstrap cards

---
+++ src/components/filters/FiltersSidebar.jsx
@@ -0,0 +1,43 @@
+import { Card, CardBody, Form, FormCheck } from "react-bootstrap";
+
+function FiltersSidebar() {
+  return (
+    <>
+      {/* Sidebar Title */}
+      <h4 className="mb-3">Filters</h4>
+
+      {/* Category Filter */}
+      <Card className="mb-3">
+        <Card.Body>
+          <Card.Title>Category</Card.Title>
+          <Form.Check type="checkbox" label="Electronics" />
+          <Form.Check type="checkbox" label="Jewelery" />
+          <Form.Check type="checkbox" label="Women's clothing" />
+          <FormCheck type="checkbox" label="Men's clothing" />
+        </Card.Body>
+      </Card>
+
+      {/* Price Filter */}
+      <Card>
+        <Card.Body>
+          <Card.Title>Price</Card.Title>
+          <Form.Check type="checkbox" label="Under $50" />
+          <Form.Check type="checkbox" label="$50 - $100" />
+        </Card.Body>
+      </Card>
+
+      {/* Rating Filter */}
+      <Card>
+        <Card.Body>
+          <Card.Title>Rating</Card.Title>
+          <Form.Check type="checkbox" label="4★ & above" />
+          <Form.Check type="checkbox" label="3★ & above" />
+          <Form.Check type="checkbox" label="2★ & above" />
+          <Form.Check type="checkbox" label="1★ & above" />
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
+
+export default FiltersSidebar;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import fetchProduct from "../services/productService";
 import ProductCard from "../components/product/ProductCard";
 import { Col, Container, Row } from "react-bootstrap";
+import FiltersSidebar from "../components/filters/FiltersSidebar";
 
 function Products() {
+  const [category, setCategory] = useState("");
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const loadProducts = async () => {
@@ -15,20 +17,32 @@ function Products() {
         console.log("Error", error);
       }
     };
-    loadProducts()
+    loadProducts();
   }, []);
-  
-  return <>
-   <Container>
-    <Row>
-      {products.map((product)=>{
-        return <Col key={product.id} xs={12} md={4} lg={3} className="mb-4">
-          <ProductCard product={product} />
-        </Col>
-      })}
-    </Row>
-   </Container>
-  </>;
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col md={3}>
+            <FiltersSidebar />
+          </Col>
+
+          <Col md={9} className="pd-4">
+            <Row>
+              {products.map((product) => {
+                return (
+                  <Col key={product.id} xs={12} md={4} lg={3} className="mb-4">
+                    <ProductCard product={product} />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Products;
